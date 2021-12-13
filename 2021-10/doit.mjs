8d2a66164f82dceb1cd35a8dc8a913ec96c86e2f
Jim B <d3x0r@users.noreply.github.com>
2021-12-13 01:41:18 -0800

    Update doit.mjs

--- 2021-10/doit.mjs
+++ 2021-10/doit.mjs
@@ -20,15 +20,12 @@ console.log( "score:", total );
 function parse(line) {
 	const stack = [];
 	for( let c of [...line] )  {
-		if( ( c === '[' )||( c === '{' )||( c === '<' )||( c === '(' )) {
+		if( "[{<(".includes(c) ) {
 			stack.push( c );
 		}
 		else {
 			const last = stack.pop();
-			if( c === ']' && last === '[' ) continue;
-			if( c === '}' && last === '{' ) continue;
-			if( c === ')' && last === '(' ) continue;
-			if( c === '>' && last === '<' ) continue;
+			if( c === lastInv[last] ) continue;
 			console.log( 'expected', lastInv[last],"Got:", c );
 			total += score[c];
 			return false;
